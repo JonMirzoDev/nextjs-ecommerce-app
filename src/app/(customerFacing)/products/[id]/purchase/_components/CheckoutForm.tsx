a3c275f89225162d4c7e0caba6087976473f30ad
JonMirzoDev <islomjon.swe@gmail.com>
2024-04-13 16:52:41 +0500

    basic purchase and checkout form pages craeted

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,11 @@
+type CheckoutFormProps = {
+  product: {}
+  clientSecret: string
+}
+
+export default function CheckoutForm({
+  product,
+  clientSecret
+}: CheckoutFormProps) {
+  return <h1>Form</h1>
+}
